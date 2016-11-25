bdd5d61831f1e256d82f5132c77c6e370d5f3ef1
valentinebaliuk <valentyn.baliuk@yahoo.com>
2016-11-25 19:58:54 +0100

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <time.h>
@@ -9,32 +10,31 @@
 #include <unistd.h>
 
 using namespace std;
+
 bool wpisane=false;
-int SmokersInBuffer=0;
-pthread_mutex_t mutex[3];//trzy muteksy, każdy odpowiada za czekanie na odpowiednie dwa składniki
-pthread_cond_t cond[3];
-pthread_mutex_t mutexme;
-pthread_cond_t condme;
-pthread_mutex_t mut;
-pthread_mutex_t thismut;
-string listOfNames[10]={"Warren","Lowell","Elliot","Chas","Trent","Elvin","Edison","Bryce","Lonny","Alex"};
-int liczbawolnych[3];
-char tc[10];//tabl charow
-int liczbaiteracji=0;
-vector<pthread_t> smokers_threads;
+int Palaczy_wBuforze=0;
+pthread_mutex_t mutexZezwoleniaNaZapis[3];//trzy muteksy, każdy odpowiada za czekanie na odpowiednie dwa składniki
+pthread_cond_t condZezwoleniaNaZapis[3];
+pthread_mutex_t mutexKontroliLosowania;
+pthread_cond_t condKontroliLosowania;
+pthread_mutex_t KontrolZapisuDoBufora;
+string ListaImion[10]={"Warren","Lowell","Elliot","Chas","Trent","Elvin","Edison","Bryce","Lonny","Alex"};
+int liczbawolnych[3];//liczba wolnych elementów każdego zasobu
+int liczbaiteracji=0;//ile razy losujemy w programie
+vector<pthread_t> wantkiPalaczy;
 
 
 struct Smoker{
 	string name;
-	int id; 	
+	int identyfikator; 	
 	int resources;//wartość-to jakiś ze składników
-   	bool inBuffer;
+   	bool Czy_wBuforze;
         Smoker(int num){
-		id=num;
-           	name=listOfNames[rand()%10];
+		identyfikator=num;
+           	name=ListaImion[rand()%10];
                 resources=rand()%3;
-		printf("%d  %d \n",id,resources);
-	        inBuffer=false;
+		cout<<identyfikator<<" "<<name<<" "<<resources<<"\n";
+	        Czy_wBuforze=false;
         }
 };
 vector<Smoker> smokers;
@@ -42,18 +42,18 @@ vector<Smoker> readyToSmoke;
 void* bufChecker(void *){
 	while(true){
 		if(liczbaiteracji==5) return 0;
-		if(SmokersInBuffer<4){wpisane=false;}
-		while(SmokersInBuffer>=4 && wpisane){
-			pthread_mutex_lock(&mut);
-			puts("pop");
+		if(Palaczy_wBuforze<4){wpisane=false;}//ustalamy ze czyścimy bufor, kiedy palaczy jest tam >=4
+		while(Palaczy_wBuforze>=4 && wpisane){
+			pthread_mutex_lock(&KontrolZapisuDoBufora);
+			cout<<"Usuwamy z przepelnionego buforu"<<"\n";
 			for(int i=0;i<4;i++){			
-				printf("%d %d\n",readyToSmoke[0].id,readyToSmoke[0].resources);
-				smokers[readyToSmoke[0].id].inBuffer=false;
-				liczbawolnych[smokers[readyToSmoke[0].id].resources]++;
+				cout<<readyToSmoke[0].identyfikator<<" "<<readyToSmoke[0].name<<" "<<readyToSmoke[0].resources<<"\n";
+				smokers[readyToSmoke[0].identyfikator].Czy_wBuforze=false;
+				liczbawolnych[smokers[readyToSmoke[0].identyfikator].resources]++;
                         	readyToSmoke.erase(readyToSmoke.begin());
 			}
-			SmokersInBuffer=SmokersInBuffer-4;		
-			pthread_mutex_unlock(&mut);
+			Palaczy_wBuforze=Palaczy_wBuforze-4;		
+			pthread_mutex_unlock(&KontrolZapisuDoBufora);
 			
 		}
 	}
@@ -65,14 +65,14 @@ void* barmen(void *){
 		int resources=rand()%3;
 	//broadcast wysyla sygnal do wszystkich kwalifikujacych sie palaczy
 		switch(resources){
-            		case 0: pthread_cond_broadcast(&cond[0]);
+            		case 0:/*cout<<"Wylosowaliśmy 0\n";*/ pthread_cond_broadcast(&condZezwoleniaNaZapis[0]);
             		break;
-            		case 1: pthread_cond_broadcast(&cond[1]);
+            		case 1:/*cout<<"Wylosowaliśmy 1\n";*/ pthread_cond_broadcast(&condZezwoleniaNaZapis[1]);
             		break;
-            		case 2: pthread_cond_broadcast(&cond[2]);
+            		case 2:/*cout<<"Wylosowaliśmy 2\n";*/ pthread_cond_broadcast(&condZezwoleniaNaZapis[2]);
             		break;
       		}
-		pthread_cond_wait(&condme,&mutexme);
+		pthread_cond_wait(&condKontroliLosowania,&mutexKontroliLosowania);
 	}
 	return 0;
 }
@@ -80,23 +80,21 @@ void* barmen(void *){
 void* func(void *smoker){
 	struct Smoker *smoker_in_func =(struct Smoker *) smoker;
 	while(true){
-		pthread_cond_wait(&cond[smoker_in_func->resources],&mutex[smoker_in_func->resources]);
-		pthread_mutex_lock(&thismut);		
-		pthread_mutex_lock(&mut);		
+		pthread_cond_wait(&condZezwoleniaNaZapis[smoker_in_func->resources],&mutexZezwoleniaNaZapis[smoker_in_func->resources]);	
+		pthread_mutex_lock(&KontrolZapisuDoBufora);		
 		
-		if(smokers[smoker_in_func->id].inBuffer==false){	
-			printf("%d Zapisuje\n",smoker_in_func->id);	
+		if(smokers[smoker_in_func->identyfikator].Czy_wBuforze==false){	
+			cout<<"Zapisuje "<<smoker_in_func->identyfikator<<" "<<smoker_in_func->name<<"\n";	
 			readyToSmoke.push_back(*smoker_in_func);
- 			smoker_in_func->inBuffer=true;
-			SmokersInBuffer++;
+ 			smoker_in_func->Czy_wBuforze=true;
+			Palaczy_wBuforze++;
 			liczbawolnych[smoker_in_func->resources]--;
-			printf("%d %d\n",liczbawolnych[smoker_in_func->resources],smoker_in_func->resources);
+			cout<<"Zostalo "<<liczbawolnych[smoker_in_func->resources]<< " zasoby typu "<<smoker_in_func->resources<<"\n";
 			
 		}
-		if(liczbawolnych[smoker_in_func->resources]==0) {/*printf("wysylam sygnal\n")*/;pthread_cond_signal(&condme);wpisane=true;}
+		if(liczbawolnych[smoker_in_func->resources]==0) {pthread_cond_signal(&condKontroliLosowania);wpisane=true;}
 		
-		pthread_mutex_unlock(&thismut);
-		pthread_mutex_unlock(&mut);
+		pthread_mutex_unlock(&KontrolZapisuDoBufora);
 	}
 	return 0;
 }
@@ -105,7 +103,8 @@ void* func(void *smoker){
 int main(){
 	srand(time(NULL));
         int NumOfSmokers;
-	scanf("%d", &NumOfSmokers);//cin>>NumOfSmokers;
+	cout<<"Ile w barze jest palaczy?\n";
+	cin>>NumOfSmokers;
 
 	/*tworzenie vektoru losowych palaczy*/
 	for(int i=0;i<NumOfSmokers;i++){
@@ -119,8 +118,8 @@ int main(){
 	/*tworzenie wątków losowych palaczy*/ 
 	for(int i=0;i<NumOfSmokers;i++){
 		pthread_t SmokerThread;
-		smokers_threads.push_back(SmokerThread);
-		pthread_create(&smokers_threads[i],NULL,func,(void*)&smokers[i]);
+		wantkiPalaczy.push_back(SmokerThread);
+		pthread_create(&wantkiPalaczy[i],NULL,func,(void*)&smokers[i]);
 
 	}
 	
